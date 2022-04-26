610ba4c1d61225b4f0e3463ebd81d4a3f4eace1b
Shoko118 <secast118@gmail.com>
2022-04-26 14:29:45 +0800

    update navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,22 +8,17 @@
       <div
         class="bg-slate-600 px-2 py-2 md:px-3 md:py-2 rounded-md hover:bg-slate-500 transform ease-in-out duration-150"
       >
-        <h1 class="font-bold text-xs md:text-xl">Todo App</h1>
+        <router-link to="/" class="font-bold text-xs md:text-xl">Todo App</router-link>
       </div>
       <div
         class="bg-slate-600 px-2 py-2 md:px-3 md:py-2 rounded-md hover:bg-slate-500 transform ease-in-out duration-150"
       >
-        <h1 class="font-bold text-xs md:text-xl">Hangman</h1>
+        <router-link to="/hangman" class="font-bold text-xs md:text-xl">Hangman</router-link>
       </div>
       <div
         class="bg-slate-600 px-2 py-2 md:px-3 md:py-2 rounded-md hover:bg-slate-500 transform ease-in-out duration-150"
       >
-        <h1 class="font-bold text-xs md:text-xl">Shop Cart</h1>
-      </div>
-      <div
-        class="bg-slate-600 px-1 py-2 md:px-3 md:py-2 rounded-md hover:bg-slate-500 transform ease-in-out duration-150"
-      >
-        <h1 class="font-bold text-xs md:text-xl">State Management</h1>
+        <router-link to="/tic-tac-toe" class="font-bold text-xs md:text-xl">Tic Tac Toe</router-link>
       </div>
     </div>
   </div>
